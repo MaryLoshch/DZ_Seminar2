dc886de750aef1c0580d6868f834b1b20e03f1e8
мария <oops.mari@mail.ru>
2022-11-28 20:21:24 +0300

    Задача 10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+string array = Convert.ToString(num);
+
+Console.WriteLine($"Вторая цифра введенного числа: {array[1]}");
