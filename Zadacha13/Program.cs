90b967be25dc5930e4e7bb64fc9f115d2b3fa9cc
мария <oops.mari@mail.ru>
2022-11-29 02:00:32 +0300

    задача 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,37 @@
+﻿// Работает до 99999
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    if (num > 999)
+    {
+        if (num > 9999)
+        {
+            if (num > 99999)
+            {
+                Console.WriteLine("Слишком большое число, здесь не справиться без строк :(");
+            }
+            else
+            {
+                int third = (num / 100) % 10;
+                Console.WriteLine($"Третья цифра вашего числа: {third}");
+            }
+        }
+        else
+        {
+            int third = (num / 10) % 10;
+            Console.WriteLine($"Третья цифра вашего числа: {third}");
+        }
+
+
+    }
+    else
+    {
+        int third = num % 10;
+        Console.WriteLine($"Третья цифра вашего числа: {third}");
+    }
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
