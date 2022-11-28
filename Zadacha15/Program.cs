5dcb1012fe7cf9f7ce8a4a6ebb2f6ba1a91b28be
мария <oops.mari@mail.ru>
2022-11-29 02:15:33 +0300

    задача 15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели (от 1 до 7):  ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 5)
+{
+    if (num > 7)
+    {
+        Console.WriteLine("Попробуй еще раз выбрать число от 1 до 7 (включительно)");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    if(num < 1) 
+    {
+        Console.WriteLine("Попробуй еще раз выбрать число от 1 до 7 (включительно)");
+    }
+    else 
+    {
+        Console.WriteLine("Нет");
+    }
+}
